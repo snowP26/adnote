fb95ab35edcb45d70a59d811ca171701773821e9
snowP_ <jverceluz@gbox.adnu.edu.ph>
2024-05-18 02:41:37 +0800

    Untrack adnote2024 directory

--- adnote2024/urls.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('login.urls')),
-    path('accounts/', include('django.contrib.auth.urls')),  # Include Django's built-in authentication URLs
-]
